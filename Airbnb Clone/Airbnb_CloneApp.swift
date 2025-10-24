0b6af89c2f9ed7b5b6fc5e01e0112b9e0fd99ce2
Sophea Pov <pov.sophea3@gmail.com>
2025-10-24 14:29:57 +0700

    Initial Commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Sophea Pov on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
