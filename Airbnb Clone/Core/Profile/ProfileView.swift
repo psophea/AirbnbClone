c5fd1b578854f0c55b49f9c3ab09cba207f80ee5
Sophea Pov <pov.sophea3@gmail.com>
2025-10-25 19:35:34 +0700

    finished ProfileView, ProfileOptionRowView, and WishlistsView

---
+++ Airbnb Clone/Core/Profile/ProfileView.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileView.swift
+//  Airbnb Clone
+//
+//  Created by Sophea Pov on 25/10/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            // profile login view
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Profile")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    
+                    Text("Log in to start planning your next trip.")
+                }
+                
+                Button {
+                    print("log in")
+                } label: {
+                    Text("Log in")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                HStack{
+                    Text("Don't have an account?")
+                    
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                .font(.caption)
+            }
+            
+            // profile options
+            VStack(spacing: 24){
+                ProfileOptionRowView(imageName: "gear", title: "Settings")
+                ProfileOptionRowView(imageName: "gear", title: "Accessibility")
+                ProfileOptionRowView(imageName: "questionmark.circle", title: "Visit the Help Center")
+            }
+            .padding(.vertical, 24)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileView()
+}
